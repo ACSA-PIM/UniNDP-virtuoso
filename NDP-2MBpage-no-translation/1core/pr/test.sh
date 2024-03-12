be6f1dd687566d079ff74f233bb4b42e0bae1414
Qingcai Jiang <jqc@mail.ustc.edu.cn>
2024-03-12 11:20:26 +0100

    Remove useless files

--- NDP-2MBpage-no-translation/1core/pr/test.sh
+++
@@ -1,6 +0,0 @@
-sniper=/root/codes/Victima/sniper/run-sniper
-config=/root/codes/Victima/sniper/config/UniNDP/NDP/baseline_NDP_1core.cfg
-# trace=/root/codes/Victima/traces_small/GraphBIG_PageRank_amazon0302.sift
-trace=/root/codes/Victima/traces/pr.sift
-
-$sniper -c $config --traces=$trace
--- NDP-no-translation/8core-2000M/sssp/sim.scripts.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.argv = [ "/root/codes/Victima/sniper/scripts/stop-by-icount.py", "2000000000" ]
-execfile("/root/codes/Victima/sniper/scripts/stop-by-icount.py")
